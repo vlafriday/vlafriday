502014a173bf8795c154f40da2ba24b4bc65f31b
vlafriday <vlafriday@mail.ru>
2025-03-27 08:39:14 +0300

    test greeting word

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Привет,я тестировщик,Привет,я исследователь,Привет,я энтузиаст,Просто добро пожаловать в мой мир...'
+    'аоптвоатвоав'
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
