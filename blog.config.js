5f24179665852d614cb1bd5eb6d778f3f16f50f6
vlafriday <vlafriday@mail.ru>
2025-03-27 08:33:19 +0300

    greeting words

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Привет, я тестировщик,Привет, я исследователь,Привет, я энтузиаст,Просто добро пожаловать в мой мир...',
+    'Привет,я тестировщик,Привет,я исследователь,Привет,я энтузиаст,Просто добро пожаловать в мой мир...',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
